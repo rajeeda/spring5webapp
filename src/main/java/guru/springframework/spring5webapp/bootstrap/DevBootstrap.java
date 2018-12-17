c69eb73f5c14c7bd58548cb429b35dcc582622f2
Nayana Hettiarachchi <nayana@mainstation.local>
2018-12-17 11:21:06 +0700

    assingment 1

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,6 +2,7 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
 import org.springframework.context.ApplicationListener;
@@ -31,7 +32,8 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
         //Eric
         Author eric = new Author("Eric", "Evans");
-        Book  ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+        Publisher publisher = new Publisher("name", "address");
+        Book  ddd = new Book("Domain Driven Design", "1234", publisher);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
@@ -41,7 +43,7 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
         //Rod
         Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE Development without EJB", "23444", "Wrox" );
+        Book noEJB = new Book("J2EE Development without EJB", "23444", publisher);
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
